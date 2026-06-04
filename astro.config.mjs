b678b4392ee36e665e8961eb27e9e184c4034889
Phinneas <buzzuw2@gmail.com>
2026-06-04 10:43:42 -0700

    initial commit/build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://portage.pages.dev',
+  output: 'static',
+  compressHTML: true,
+  integrations: [sitemap()],
+});
